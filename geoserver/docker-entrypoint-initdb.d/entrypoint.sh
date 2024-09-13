53b71047a4fde6f3f48775d4c1e64aedc7171eb4
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-09-13 10:03:56 +0200

    HTM-1209: Some last updates after migration this to a Tailormap repo
    - fix dependabot labels
    
    - fixup workflows
    
    - add the missing no-traefik.yml override

--- geoserver/docker-entrypoint-initdb.d/entrypoint.sh
+++ geoserver/docker-entrypoint-initdb.d/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+JAVA_OPTS="-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom"
 # start tomcat, start the script to update the passwords
 /docker-entrypoint-initdb.d/0-reset_passwords.sh &
 catalina.sh run
