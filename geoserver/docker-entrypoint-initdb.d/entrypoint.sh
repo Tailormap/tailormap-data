f0a529057f953efcd04555169e7dbbbe4d550b86
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-09-26 17:35:55 +0200

    HTM-1238: Update tomcat and use the Marlin renderer (#6)
    Also use the new RelateNG options
    
    And remove settings from entrypoint JAVA_OPTS

--- geoserver/docker-entrypoint-initdb.d/entrypoint.sh
+++ geoserver/docker-entrypoint-initdb.d/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
-JAVA_OPTS="-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom"
+
+#JAVA_OPTS=
 # start tomcat, start the script to update the passwords
 /docker-entrypoint-initdb.d/0-reset_passwords.sh &
 catalina.sh run
