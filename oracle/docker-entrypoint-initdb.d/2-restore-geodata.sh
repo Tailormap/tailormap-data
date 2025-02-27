68c5628bb83fa0db618b91f121039195aa7292f4
Matthijs Laan <matthijsln@gmail.com>
2025-02-27 17:11:55 +0100

    Try some extra import dump output, use 23.6

--- oracle/docker-entrypoint-initdb.d/2-restore-geodata.sh
+++ oracle/docker-entrypoint-initdb.d/2-restore-geodata.sh
@@ -4,4 +4,10 @@ set -e
 echo "app user password "${APP_USER_PASSWORD}"
 echo "geodata user password "${GEODATA_PASSWORD}"
 
+printf 'Start import dump at %(%T)T\n' -1
 impdp geodata/"${APP_USER_PASSWORD}"@localhost:1521/FREEPDB1 DUMPFILE=dumpdir:geodata.dump LOGFILE=dumpdir:geodata.log
+printf 'Finished import dump at %(%T)T, exit code %d\n\n' -1 $?
+echo Logfile:
+
+cat /tmp/dumpdir/geodata.log
+
